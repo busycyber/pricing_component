4a48f2b0b274a05272e31136c61c972af0b44ffe
aristeoibarra <aristeoibarra608@gmail.com>
2023-04-29 19:18:55 -0700

    feat: Added assets, images, fake API, and global styles
    This commit adds various assets, images, and a fake API for testing purposes. It also includes global styles for consistent styling across the application.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          light: '#A2A7F0',
+          dark: ' #696EDD'
+        },
+        neutral: {
+          light: '#F7F7FE',
+          medium: '#6E728E',
+          dark: '#4A4D60'
+        }
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif']
+      }
+    },
+    screens: {
+      mobile: '375px',
+      desktop: '1440px'
+    }
   },
   plugins: []
 }
