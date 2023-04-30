ede1a541cc825992a4aab92d027bd051329992ef
aristeoibarra <aristeoibarra608@gmail.com>
2023-04-29 19:28:20 -0700

    feat: Add App component and integrate Card and PricingSwitcher components
    Added App component using Card and PricingSwitcher components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+import { Card, PricingSwitcher } from '@/components';
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="px-5 desktop:px-[195px]">
+    <PricingSwitcher />
+    <div class="mt-20 mb-[71px] flex flex-col gap-8 desktop:flex-row desktop:gap-0">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </div>
 </template>|
