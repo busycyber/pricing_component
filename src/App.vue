c38be9e8ea62e1360b01d0bc81e8b51e4e3dd4c7
aristeoibarra <aristeoibarra608@gmail.com>
2023-04-30 17:12:36 -0700

    feat: Added plans data fetching and rendered plan cards using fetched data
    Fetched plans data from the API using planRepository and rendered plan cards using Card component with the fetched data.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import { Card, PricingSwitcher } from '@/components';
+import { planRepository } from '@/data/repositories';
+import { onMounted, ref } from 'vue';
+import type { PlanModel } from './data/models';
+
+const plansData = ref<PlanModel[]>([]);
+
+onMounted(async () => {
+  await planRepository.getPlans()
+    .then((response) => plansData.value = response)
+    .catch((error) => console.log(error));
+});
 </script>
 
 <template>
-  <div class="px-5 desktop:px-[195px]">
-    <PricingSwitcher />
-    <div class="mt-20 mb-[71px] flex flex-col gap-8 desktop:flex-row desktop:gap-0">
-      <Card />
-      <Card />
-      <Card />
+  <div class="container">
+    <div class="px-5 desktop:px-[195px]">
+      <PricingSwitcher />
+      <div class="mt-20 mb-[71px] flex flex-col gap-8 items-center desktop:flex-row desktop:gap-0">
+        <Card v-for="(plan, index) in plansData" :key="plan.id" :index="index" :name="plan.name"
+          :priceMonthly="plan.priceMonthly" :priceAnnually="plan.priceAnnually" :features="plan.features"
+          :top="plan.top" />
+      </div>
     </div>
   </div>
-</template>|
+</template>
